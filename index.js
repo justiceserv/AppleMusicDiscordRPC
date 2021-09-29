901b592fee13a2e939619d312fef08fbf59a3b77
Justiceserv <justiceserv@jserv.xyz>
2021-09-29 16:24:09 +0900

    Auto Update

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const iTunes = require('itunes-bridge');
 const RPC = require('discord-rpc')
 const { searchSong } = require('@tbogard/itunes-search');
 const iTunesEmitter = iTunes.emitter;
+const { AutoGitUpdate } = require('auto-git-update');
+const config = {
+    repository: 'https://github.com/justiceserv/MacOSDiscordRPC', 
+    tempLocation: './tempupdate/'
+}
+const update = new AutoGitUpdate(config); 
+update.autoUpdate(); 
+
 let client = new RPC.Client({ transport: 'ipc' })
 client.login({ clientId: '891586647790075964' }).catch(console.error);
-iTunesEmitter.on('playing', function (type, CurrentTrack) {
+iTunesEmitter.on('playing', function (CurrentTrack) {
     searchSong(`${CurrentTrack.artist} - ${CurrentTrack.name}`).then((result) => {
-        client.on('ready', () => {})
+        client.on('ready', () => { })
         client.request('SET_ACTIVITY', {
             pid: process.pid,
             activity:
             {
                 details: `Playing ${CurrentTrack.name}`,
                 state: `Album: ${CurrentTrack.album}`,
-                timestamps: 
+                timestamps:
                 {
-                    end: Date.now() + CurrentTrack.remainingTime * 1000,
+                    end: Date.now() + CurrentTrack.remainingTime * 1000 + 1,
                 },
                 assets:
                 {
@@ -23,7 +31,7 @@ iTunesEmitter.on('playing', function (type, CurrentTrack) {
                     large_text: 'test',
                     small_image: `applemusic`,
                     small_text: 'test',
-                    
+
                 },
                 buttons: [
                     {
@@ -36,8 +44,10 @@ iTunesEmitter.on('playing', function (type, CurrentTrack) {
     console.log(CurrentTrack)
 })
 iTunesEmitter.on('paused', function () {
-    client.request('SET_ACTIVITY', {pid: process.pid});
+    client.request('SET_ACTIVITY', { pid: process.pid });
+    console.log(`${CurrentTrack.name} is stopped`)
 })
 iTunesEmitter.on('stopped', function () {
-    client.request('SET_ACTIVITY', {pid: process.pid});
+    client.request('SET_ACTIVITY', { pid: process.pid });
+    console.log(`Apple Music is stopped.`)
 })

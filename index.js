48b566b66ea180e7c701733d7f0ec258e4388fe6
Justiceserv <justiceserv@jserv.xyz>
2021-09-29 16:13:26 +0900

    Almsot Done

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-const iTunes = require('itunes-bridge'); 
-const rpcclient = require('discord-rich-presence')('891586647790075964'); 
-const { searchSong } = require('@tbogard/itunes-search'); 
-const iTunesEmitter = iTunes.emitter; 
-iTunesEmitter.on('playing', function(type, CurrentTrack)
-{
-    searchSong(`${CurrentTrack.artist} - ${CurrentTrack.name}`).then((result)=>
-    //result.results[0].trackViewUrl
-        rpcclient.updatePresence({
-            state: `Album: ${CurrentTrack.album}`,
-            details: `Playing ${CurrentTrack.name}`, 
-            startTimestamp: CurrentTrack.elapsedTime, 
-            largeImageKey: `defaultgenre`, 
-            smallImageKey: `maclogo`
+const iTunes = require('itunes-bridge');
+const RPC = require('discord-rpc')
+const { searchSong } = require('@tbogard/itunes-search');
+const iTunesEmitter = iTunes.emitter;
+let client = new RPC.Client({ transport: 'ipc' })
+client.login({ clientId: '891586647790075964' }).catch(console.error);
+iTunesEmitter.on('playing', function (type, CurrentTrack) {
+    searchSong(`${CurrentTrack.artist} - ${CurrentTrack.name}`).then((result) => {
+        client.on('ready', () => {})
+        client.request('SET_ACTIVITY', {
+            pid: process.pid,
+            activity:
+            {
+                details: `Playing ${CurrentTrack.name}`,
+                state: `Album: ${CurrentTrack.album}`,
+                timestamps: 
+                {
+                    end: Date.now() + CurrentTrack.remainingTime * 1000,
+                },
+                assets:
+                {
+                    large_image: `defaultgenre`,
+                    large_text: 'test',
+                    small_image: `applemusic`,
+                    small_text: 'test',
+                    
+                },
+                buttons: [
+                    {
+                        label: 'Listen On Apple Music', url: `${result.results[0].trackViewUrl}`
+                    },
+                ]
+            }
         })
-    ); 
+    })
     console.log(CurrentTrack)
 })
-iTunesEmitter.on('paused', function(type, currentTrack)
-{
-    rpcclient.updatePresence({});
+iTunesEmitter.on('paused', function () {
+    client.request('SET_ACTIVITY', {pid: process.pid});
 })
-iTunesEmitter.on('stopped', function()
-{
-    
+iTunesEmitter.on('stopped', function () {
+    client.request('SET_ACTIVITY', {pid: process.pid});
 })

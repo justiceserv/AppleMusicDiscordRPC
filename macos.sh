9a114e44281957be1338c3f2f97d8db404f185c0
Justiceserv <justiceserv@jserv.xyz>
2021-09-30 10:39:33 +0900

    MacOS Support

--- macos.sh
+++ macos.sh
@@ -4,7 +4,8 @@ if which node > /dev/null
     then 
         echo "Node installed, skipping node install process."
     else 
-        curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+        curl -o node-v14.pkg https://nodejs.org/dist/v14.18.0/node-v14.18.0.pkg
+        sudo installer -verbose -pkg node-v14.pkg -target /
     fi 
 npm i 
 npm start

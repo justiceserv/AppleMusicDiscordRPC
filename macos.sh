6510e41c82ddabc2ae0b4422273325e071adf5ed
Justiceserv <justiceserv@jserv.xyz>
2021-09-30 10:25:29 +0900

    Fix MacOS Script

--- macos.sh
+++ macos.sh
@@ -4,15 +4,8 @@ if which node > /dev/null
     then 
         echo "Node installed, skipping node install process."
     else 
-        echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-        . ~/.bashrc
-        mkdir ~/local
-        mkdir ~/node-latest-install
-        cd ~/node-latest-install
-        curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-        ./configure --prefix=~/local
-        make install # ok, fine, this step probably takes more than 30 seconds...
-        curl https://www.npmjs.org/install.sh | sh
+        curl -o node-v14.pkg https://nodejs.org/dist/v14.18.0/node-v14.18.0.pkg
+        installer -pkg node-v14.pkg -target CurrentUserHomeDirectory
     fi 
 npm i 
 npm start

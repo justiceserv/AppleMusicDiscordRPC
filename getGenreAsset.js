53231cc8a50ae67b6f59e3b9ce251a960013f5df
Justiceserv <justiceserv@jserv.xyz>
2021-09-30 11:40:59 +0900

    IU Support

--- getGenreAsset.js
+++ getGenreAsset.js
@@ -4,7 +4,28 @@ exports.getGenreAssets = function (album, genreinput, artist) {
     let temp = genreinput.toLowerCase();
     switch (artist) {
         case 'IU':
-            return switchGenre(temp);
+            switch(album){
+                case 'Modern Times': 
+                    return 'iumoderntimes'
+                case 'Palette': 
+                    return 'iupalette'
+                case `IU 5th Album 'LILAC'`:
+                    return 'iulilac'
+                case 'CHAT-SHIRE':
+                    return 'iuchatshire'
+                case 'A Flower Bookmark': 
+                    return 'iuflower'
+                case 'Last Fantasy': 
+                    return 'iulast'
+                case 'Real': 
+                    return 'iureal'
+                case 'IU...IM':
+                    return 'im'
+                case 'Growing up':
+                    return 'growingup'
+                default: 
+                    return switchGenre(temp); 
+            }
         default:
             return switchGenre(temp);
     }

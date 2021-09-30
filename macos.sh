403f7e319f3e00de75bcc1af7adbb1e7a8c5f0fb
Justiceserv <justiceserv@jserv.xyz>
2021-09-30 10:32:59 +0900

    MacOS Support

--- macos.sh
+++ macos.sh
@@ -4,8 +4,7 @@ if which node > /dev/null
     then 
         echo "Node installed, skipping node install process."
     else 
-        curl -o node-v14.pkg https://nodejs.org/dist/v14.18.0/node-v14.18.0.pkg
-        installer -pkg node-v14.pkg -target CurrentUserHomeDirectory
+        curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
     fi 
 npm i 
 npm start

1bbf00f50e5cd38054e7b14afe52585738233e89
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-11-11 13:25:23 -0500

    Standardize whitespace on typical conventions
    * JS & JSON files typically use 2-space indentation
    * EditorConfig helps automate whitespace settings in editors
        * https://editorconfig.org

--- getGenreAsset.js
+++ getGenreAsset.js
@@ -1,25 +1,25 @@
 var exports = module.exports = {};
 
 exports.getGenreAssets = function (album, genreinput, artist) {
-    var temp = genreinput.toLowerCase();
-    var iualbumdata = require('./datas/iualbums.json')
-    if (artist.includes('IU')) {
-        if(!album || !iualbumdata[album]) {
-            return 'iudefault'
-        }
-        console.log(iualbumdata[album])
-        return iualbumdata[album]
+  var temp = genreinput.toLowerCase();
+  var iualbumdata = require('./datas/iualbums.json')
+  if (artist.includes('IU')) {
+    if(!album || !iualbumdata[album]) {
+      return 'iudefault'
     }
-    else { return switchGenre(temp) }
+    console.log(iualbumdata[album])
+    return iualbumdata[album]
+  }
+  else { return switchGenre(temp) }
 }
 
 function switchGenre(genrelower) {
-    var albumdata = require('./datas/genre.json')
-    if (!genrelower || !albumdata[genrelower]) {
-        return 'defaultgenre'
-    }
-    else {
-        console.log(albumdata[genrelower])
-        return albumdata[genrelower]
-    }
+  var albumdata = require('./datas/genre.json')
+  if (!genrelower || !albumdata[genrelower]) {
+    return 'defaultgenre'
+  }
+  else {
+    console.log(albumdata[genrelower])
+    return albumdata[genrelower]
+  }
 }
